36a902839270fa135928827000ef4ba926cbff75
msklnk <moskalenko-lena@inbox.ru>
2024-10-23 00:20:27 +0400

    Добавлены/изменены файлы

--- app/schemas.py
+++ app/schemas.py
@@ -2,33 +2,26 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import date
 
-
 class ClientBase(BaseModel):
     name: str
     phone_number: Optional[str] = None
     mail: Optional[str] = None
     discount_percentage: Optional[int] = None
 
-
 class Client(ClientBase):
     clientid: int
-
     class Config:
         orm_mode = True
-
 
 class DishBase(BaseModel):
     name: str
     type: Optional[str] = None
     recipe: Optional[str] = None
 
-
 class Dish(DishBase):
     dishid: int
-
     class Config:
         orm_mode = True
-
 
 class OrderBase(BaseModel):
     tableid: Optional[int] = None
@@ -39,10 +32,7 @@ class OrderBase(BaseModel):
     clientid: Optional[int] = None
     payment_method: Optional[str] = None
 
-
 class Order(OrderBase):
     orderid: int
-
     class Config:
         orm_mode = True
-
