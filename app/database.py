b74f006a85a3f7a4f9e1b631d5b1e7961e84b3fa
msklnk <moskalenko-lena@inbox.ru>
2024-10-22 22:42:15 +0400

    Изменены имя пользователя, пароль, хост и название базы данных в строке SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres/postgres"

--- app/database.py
+++ app/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db/dbname"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
