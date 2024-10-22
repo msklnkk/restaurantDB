53292c9411d2ed7ee29bdeea602fae7b92437494
msklnk <moskalenko-lena@inbox.ru>
2024-10-22 13:26:08 +0400

    Определены модели данных для клиентов, блюд и заказов
    - Создана модель Client для хранения информации о клиентах
    - Создана модель Dish для представления блюд в меню
    - Создана модель Order для отслеживания заказов
    - Добавлены основные поля и связи между таблицами
    - Использованы типы данных SQLAlchemy для определения структуры таблиц

---
+++ app/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, Numeric, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Client(Base):
+    __tablename__ = "clients"
+    clientid = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    phone_number = Column(String)
+    mail = Column(String)
+    discount_percentage = Column(Integer)
+
+class Dish(Base):
+    __tablename__ = "dishes"
+    dishid = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    type = Column(String)
+    recipe = Column(String)
+
+class Order(Base):
+    __tablename__ = "orders"
+    orderid = Column(Integer, primary_key=True, index=True)
+    tableid = Column(Integer, ForeignKey("tables.tableid"))
+    order_date = Column(Date)
+    total_sum = Column(Numeric)
+    status = Column(String)
+    staffid = Column(Integer, ForeignKey("staff.staffid"))
+    clientid = Column(Integer, ForeignKey("clients.clientid"))
+    payment_method = Column(String)
